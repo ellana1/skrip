69f294051bd1408aa427d7bcaa20d474b3525e6c
Elin M <62458524+repost-em@users.noreply.github.com>
2021-08-09 11:28:15 +0700

    Delete admin directory

--- admin/kategori/delete_kategori.php
+++
@@ -1,8 +0,0 @@
-<?php
-$id = @$_GET['id'];
-mysqli_query($conn, "DELETE FROM kategori WHERE id = '$id'") or die(mysqli_error($conn));
-?>
-<script type="text/javascript">
-    alert("Data Berhasil Dihapus");
-    window.location.href = "index.php?halaman=kategori";
-</script>
